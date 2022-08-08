64776e78afa200286320e49cc0b952b5f0dae8f3
liaHi19 <gn.natalia19@gmail.com>
2022-08-08 20:11:08 +0300

    [#1] Add basic functionality of react-query

---
+++ pages/app/services/country.service.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:5000";
+
+axios.defaults.baseURL = API_URL;
+
+export interface ICountry {
+  id: number;
+  title: string;
+  population: string;
+  image: "string";
+}
+
+export const CountryService = {
+  async getAll() {
+    return axios.get<ICountry[]>(`/countries`);
+  },
+};
